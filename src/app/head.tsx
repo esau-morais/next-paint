781a640838bb89ad4e0460889e6a905984289866
esau-morais <esaumorais7@gmail.com>
2022-12-13 20:23:44 -0300

    feat: setup first look

---
+++ src/app/head.tsx
@@ -0,0 +1,12 @@
+const Head = () => {
+  return (
+    <>
+      <title>Next Paint</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="Paint: The best way to have fun by drawing with friends!" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  )
+}
+
+ export default Head
